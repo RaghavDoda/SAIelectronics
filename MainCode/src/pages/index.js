cf960b9a18255cd7587c6afd2589d710869ba2bf
Raghav <raghav03.doda@gmail.com>
2023-06-27 12:31:19 +0530

    intialised project and started home page

---
+++ MainCode/src/pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/navbar'
+import Offers from '../components/offers'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Offers/>
+    </>
+  )
+}
---
+++ MainCode/src/pages/services.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar'
+
+export default function Home() {
+  return (
+    <>
+        <Navbar/>
+        <h1>Hello Services</h1>
+    </>
+  )
+}
