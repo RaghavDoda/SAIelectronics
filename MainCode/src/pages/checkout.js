69676a27dafeed10edf21d3684fc31c54d370fce
Raghav <raghav03.doda@gmail.com>
2023-06-27 13:34:58 +0530

    Major Frontend Part

---
+++ MainCode/src/pages/checkout.js
@@ -0,0 +1,13 @@
+
+import Navbar from '../components/navbar'
+
+const checkout = () => {
+    return (
+        <>
+            <Navbar/>
+            <h1>Coming soon....</h1>
+        </>
+    )
+}
+
+export default checkout
--- MainCode/src/pages/index.js
+++ MainCode/src/pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/navbar'
 import Offers from '../components/offers'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <>
       <Navbar/>
       <Offers/>
+      <Footer/>
     </>
   )
 }
